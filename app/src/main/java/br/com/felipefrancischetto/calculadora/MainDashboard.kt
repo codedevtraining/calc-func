d4adf5124f2ac3f11a09ef799f93e9f2a0fe1ae2
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-02-20 21:50:33 -0300

    chore: initial commit

---
+++ app/src/main/java/br/com/felipefrancischetto/calculadora/MainDashboard.kt
@@ -0,0 +1,12 @@
+package br.com.felipefrancischetto.calculadora
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainDashboard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_dashboard)
+    }
+}
